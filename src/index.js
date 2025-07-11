f837b9c6e1a8e889e43e721c3dbb01e9a138a46d
krupalgohil <gohilkrupal8@gmail.com>
2025-07-11 20:22:01 +0530

    solve error

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import dotenv from "dotenv"
-import mongoose, { connect } from "mongoose";
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js"; 
 import { DB_NAME } from "./constant.js";
-import connectDB from "./db/index.js";
 
-dotenv.config({
-    path:'./env'
-})
 
+dotenv.config(); 
+
+const app = express(); 
 
 connectDB()
-.then(() =>{
-    app.listen(process.env.PORT || 8000 , () => {
-        comsole.log(`SERVER is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`🚀 SERVER is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("❌ MONGO DB connection failed !!!", err);
+  });
 
 
 
